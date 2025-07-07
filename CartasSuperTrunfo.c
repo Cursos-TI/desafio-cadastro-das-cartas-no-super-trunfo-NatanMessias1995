eb2ff58a076843cd2a88fe0da90ce2ff29d7043b
NatanMessias1995 <natanmmessias15@gmail.com>
2025-07-07 20:16:38 +0000

    Carta 1 completa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,34 +9,37 @@
 int main() {
 
 
-    char nomeestado1[20], nomeestado2[20]; 
-    char codigoestado1[20], codigoestado2[20];
-    char nomecidade1[20] , nomecidade2[20];
+    char nomeestado1[50], nomeestado2[50]; 
+    char codigoestado1[50], codigoestado2[50];
+    char nomecidade1[50] , nomecidade2[50];
     float populacao1 , populacao2;
     float area1 , area2;
     float pib1,pib2;
     int pontostur1 , pontostur2;
 
-    printf("Nome do Estado : \n");
+    printf("Sigla do Estado : \n");
     scanf("%s", nomeestado1);
 
-    printf("Codigo da Cidade : \n");
-    Scanf("%s",nomecidade1);
+    printf("Nome da Cidade : \n");
+    scanf("%s", nomecidade1);
     
     printf("Populaçao cidade : \n");
-    scanf("%f", populacao1);
+    scanf("%f", &populacao1);
     
     printf("Área total : \n");
-    scanf ("%f", area1);
+    scanf ("%f", &area1);
     
     printf("PIB da cidade : \n");
-    scanf("%f", pib1);
+    scanf("%f", &pib1);
 
-    printf(" Quantidade de Pontos Turisticos : \n");
-    scanf("%d", pontostur1);
+    printf("Quantidade de Pontos Turisticos : \n");
+    scanf("%d", &pontostur1);
 
     
+    /// DADOS DA CARTA 01 ///
 
+    printf("Estado : %s\n - Cidade : %s\n - População : %f\n - Área em metros : %f\n - PIB : %f\n - Pontos Turísticos : %d\n ", nomeestado1,
+        nomecidade1, populacao1 , area1 , pib1, pontostur1 );
 
 
 
